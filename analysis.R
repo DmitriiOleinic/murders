e9aedc75b0f87567644c12149bb3e4a88848e433
dmitro777 <dmitrii.oleinic777@gmail.com>
2022-11-08 23:00:13 +0200

    changed color

--- analysis.R
+++ analysis.R
@@ -7,7 +7,7 @@ load("rda/murders.rda")
 
 murders %>% mutate(abb = reorder(abb, rate)) %>%
   ggplot(aes(abb, rate)) +
-  geom_bar(width = 0.5, stat = "identity", color = "blue") +
+  geom_bar(width = 0.5, stat = "identity", color = "red") +
   coord_flip()
 
 ggsave("figs/barplot.png")
